d2d66297bec5e3508391bcd17ab2a6c41be3d48d
justinseibert <justinseibert@users.noreply.github.com>
2017-11-19 12:15:51 -0500

    v1: uses 'touched' variable to ignore subsequent click calls

--- touchclick.js
+++ touchclick.js
@@ -1,51 +1,46 @@
 /**
- * @param {array} buttons - nested array of [element,callback] pairs
- * @param {string} element - element denoted by class ('.class') or id ('#id')
+ * this version manually ignores the click callback after a touchend is registered
+ * @param {string} element - target element denoted by class ('.class') or id ('#id')
  * @param {function} callback - the function to attach to the element
  */
 
-var TouchClick = function(buttons){
-  for (var button = 0; button < buttons.length; button++){
-    var element = buttons[button][0];
-    var callback = buttons[button][1];
+var touchclick = function(element, callback){
+  var touched = false;
+  var symbol = element[0];
+  var name = element.substr(1);
 
-    var symbol = element[0];
-    var name = element.substr(1);
+  if (symbol == '.'){
+    element = document.getElementsByClassName(name);
+  } else if (symbol == '#'){
+    element = document.getElementById(name);
+  }
 
-    if (symbol == '.'){
-      element = document.getElementsByClassName(name);
-    } else if (symbol == '#'){
-      element = document.getElementById(name);
+  if (element.length != 'undefined' && element.length > 0){
+    for (var i = 0; i < element.length; i++){
+      addTouch(element[i]);
+      addClick(element[i]);
     }
+  } else {
+    addTouch(element);
+    addClick(element);
+  }
 
-    console.log(element);
-    if (element.length != 'undefined' && element.length > 0){
-      for (var i = 0; i < element.length; i++){
-        addTouch(element[i]);
-        addClick(element[i]);
-      }
-    } else {
-      addTouch(element);
-      addClick(element);
-    }
+  function addTouch(elem){
+    elem.addEventListener('touchend', handleTouchStart, false);
+  }
+  function addClick(elem){
+    elem.addEventListener('click', handleClick, false);
+  }
 
-    function addTouch(elem){
-      var move = false;
-      elem.addEventListener('touchmove', function(evt){
-        move = true;
-      })
-      elem.addEventListener('touchend', function(evt){
-        if (!move){
-          evt.preventDefault();
-          elem.click();
-        }
-      })
+  function handleTouchStart(){
+    touched = true;
+    callback(this);
+  }
+  function handleClick(){
+    if (touched){
+      touched = false;
+      return;
     }
-
-    function addClick(elem){
-      elem.addEventListener('click', function(evt){
-        callback(elem);
-      })
-    }
+    callback(this);
   }
 }
